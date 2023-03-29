233ab88705e81472ae6b827257437f00c6826108
Towfik Ahmed <75902572+towfikahmed1029@users.noreply.github.com>
2023-03-29 11:56:29 +0600

    Update def.py

--- def.py
+++ def.py
@@ -7,3 +7,27 @@ def firstline():
             if number != 0:
                 fp.write(line)
     return lines[0].replace("\n","")
+
+def visibil_element(driver, by, selector, wait=10):
+    element = False
+    if by == 'name':
+        byselector = By.NAME
+    if by == 'xpath':
+        byselector = By.XPATH
+    if by == 'css':
+        byselector = By.CSS_SELECTOR
+    if by == 'id':
+        byselector = By.ID
+    try:
+        element = WebDriverWait(driver, wait).until(
+            EC.visibility_of_element_located((byselector, selector)))
+    except Exception as e:
+        # print(e)
+        element = False
+    if element == False:
+        pass
+        # print("visibil_element not find: ", selector)
+    else:
+        pass
+        # print(selector)
+    return element
