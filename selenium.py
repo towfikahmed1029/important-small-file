5ca861b02e658c4ce61cfd3821ae38e7b02cd875
Towfik Ahmed <75902572+towfikahmed1029@users.noreply.github.com>
2023-04-06 00:09:51 +0600

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -1,4 +1,10 @@
 import chromedriver_autoinstaller
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver.set_window_size(700,700)
 ### Auto driver install
 try:
     driver_path = chromedriver_autoinstaller.install()
