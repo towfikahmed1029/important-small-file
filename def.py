9730d279945b4391b20eac50850a50139822d885
Towfik Ahmed <75902572+towfikahmed1029@users.noreply.github.com>
2023-05-28 11:24:50 +0600

    Update def.py

--- def.py
+++ def.py
@@ -14,3 +14,27 @@ def firstline():
             if number != 0:
                 fp.write(line)
     return lines[0].replace("\n","")
+import requests
+import re
+from bs4 import BeautifulSoup
+
+headers = { 
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+}
+
+# Email Get
+def get_email(url):
+    try:
+        domain = url.split('//')[-1].replace('www.', '').split('/')[0]
+        url_gen = f'http://www.skymem.info/srch?q={domain}'
+        response = requests.get(url_gen, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        email_list = re.findall(r"href=\"\/srch\?q=(.*?@.*)\">", str(soup))
+        email = [line for line in email_list if domain in line][0]
+
+        return email
+    except:
+        #print(traceback.format_exc())
+        return None
+
+
