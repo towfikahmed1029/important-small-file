903a3b85a8697a7d4ef51d23a430680cf37cf54d
Towfik Ahmed <75902572+towfikahmed1029@users.noreply.github.com>
2023-06-17 13:14:19 +0600

    Update google sheet.py

--- google sheet.py
+++ google sheet.py
@@ -1,12 +1,13 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import pandas as pd
+### https://console.cloud.google.com/apis/credentials?project=gspython-390105
 scope = ['https://www.googleapis.com/auth/spreadsheets']
 credentials = Credentials.from_service_account_file('key.json', scopes=scope)
 client = gspread.authorize(credentials)
 spreadsheet_id = '1oofCJasXutqRY_28Z_cIRiUpMD-V9tA8FRGHz3ekY_I'
 sheet = client.open_by_key(spreadsheet_id).sheet1
 
-### https://console.cloud.google.com/apis/credentials?project=gspython-390105
 # Read data from a specific range
 def read_data():
     data = sheet.get_all_values()
@@ -35,3 +36,18 @@ def check_data():
 read_data()
 write_data()
 check_data()
+
+ #### xl to google sheet convert 
+xlsx_file_path = 'cnmv_output.xlsx'
+df = pd.read_excel(xlsx_file_path)
+
+sheet.clear() 
+# Write header to Google Sheet
+header_values = df.columns.tolist()
+sheet.update('A1', [header_values])
+
+# Write data to Google Sheet
+data_values = df.values.tolist()
+sheet.update('A2', data_values)
+
+print('Data written successfully.')
