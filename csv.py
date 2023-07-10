db6b3716a91e0c6289be9250e1d5d0c38e4f589e
Towfik Ahmed <75902572+towfikahmed1029@users.noreply.github.com>
2023-07-10 20:10:45 +0600

    Update csv.py

--- csv.py
+++ csv.py
@@ -1,5 +1,5 @@
 def save_data(data):
-    with open('data.csv', mode='a', newline='') as file:
+    with open('data.csv', mode='a', newline='',encoding='utf-8') as file:
         writer = csv.writer(file)
         if file.tell() == 0:
             writer.writerow(["URL",'Email Address','Telephone','Website'])
